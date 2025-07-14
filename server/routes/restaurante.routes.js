b702f433a05a464c72aae68c181bb1703888172d
Mathew EPN <mathew.gutierrez@epn.edu.ec>
2025-07-14 14:13:47 -0500

    Añade comentarios explicativos en el controlador y corrige un error tipográfico en la ruta para crear un nuevo restaurante

--- server/routes/restaurante.routes.js
+++ server/routes/restaurante.routes.js
@@ -1,7 +1,9 @@
 const RestauranteController = require('../controllers/restaurante.controller');
-
+//Que es una ruta de express?
+//Cual es la estructura?
+//En terminos generales, como un endpoint, un path (ruta), está el controlador, 
 module.exports = function(app) {
-    app.post("/restaurantes/neuvo", RestauranteController.CreateRestaurante);
+    app.post("/restaurantes/nuevo", RestauranteController.CreateRestaurante);
     app.get("/restaurantes", RestauranteController.getAllRestaurantes);
     app.get("/restaurantes/:id", RestauranteController.getRestaurante);
     app.put("/restaurantes/:id", RestauranteController.updateRestaurante);

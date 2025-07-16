74452a0a2d95c2cd916d3af7937ae474e889b692
Mathew EPN <mathew.gutierrez@epn.edu.ec>
2025-07-16 15:31:13 -0500

    Corrige errores de sintaxis en la configuración de Sequelize y asegura la creación de la base de datos 'Restaurante'.

--- server_mysql/config/sequelize.config.js
+++ server_mysql/config/sequelize.config.js
@@ -1,16 +1,34 @@
-const {Sequelize} = requiere("sequelize");
+const {Sequelize} = require("sequelize");
 
 const username = "root";
 const password = "root";
-const bdd_name = "myApp";
+const bdd_name = "Restaurante";
 const hostName = "localhost";
 
 //Conexión incial sin especificar la base de datos
-const initialSequelize = new Sequelize("mysql://${username}:${password}@localhost");
+const initialSequelize = new Sequelize(`mysql://${username}:${password}@localhost`);
 
-initialSequelize.query("CREATE DATABASE IF NOT EXISTS ${bdd_name};")
-.then(()=> console.log("DBB creada o ya existía"))
-.catch((error)=>{
-    console.error("Error al crear la BDD", error);
-    process.exit(1); //Termina el proceso si hay un error
-})
+initialSequelize.query(`CREATE DATABASE IF NOT EXISTS ${bdd_name};`)
+    .then(() => console.log("DBB creada o ya existía"))
+    .catch((error) => {
+        console.error("Error al crear la BDD", error);
+        process.exit(1);
+    });
+
+const sequelize = new Sequelize(bdd_name, username, password, {
+    host: hostName,
+    dialect: "mysql",
+});
+
+//EN el sync si se utiliza el parametro de entrafa force: true, se eliminan las tablas existentes y se crean de nuevo
+//Con el alter:true se actualizan las tablas existentes sin perder los datos, pero puede causar problemas si hay cambios incompatibles (Hace el mejor intento de mantener los datos)
+//Lo recomendable en el desarrollo es usar force true
+//En producción no se debe utilizar ninguno.
+
+sequelize.sync().then(() => {
+    console.log("Base de datos sincronizada");
+}).catch((error) => {
+    console.error("Error al sincronizar la base de datos", error);
+});
+
+module.exports = sequelize;
